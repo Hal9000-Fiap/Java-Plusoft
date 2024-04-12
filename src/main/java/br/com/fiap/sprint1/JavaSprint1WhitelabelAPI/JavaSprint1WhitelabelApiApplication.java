f05e614702ebfa5c2dee8370405195fb14bace30
Diogo Gallina <88459755+Diogo-gallina@users.noreply.github.com>
2024-04-12 19:56:36 -0300

    build: send initial code

---
+++ src/main/java/br/com/fiap/sprint1/JavaSprint1WhitelabelAPI/JavaSprint1WhitelabelApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.sprint1.JavaSprint1WhitelabelAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSprint1WhitelabelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSprint1WhitelabelApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/sprint1/JavaSprint1WhitelabelAPI/JavaSprint1WhitelabelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.sprint1.JavaSprint1WhitelabelAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSprint1WhitelabelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
