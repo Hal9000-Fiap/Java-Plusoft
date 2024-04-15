8d469d4cc9b01c3ab6f8060ecc92e5856e1049b9
diogo-gallina <diogogallina20@gmail.com>
2024-04-15 19:29:57 -0300

    feat: create service feedback controller

--- src/main/java/br/com/fiap/sprint1/JavaSprint1WhitelabelAPI/controller/ServiceFeedbackController.java
+++ src/main/java/br/com/fiap/sprint1/JavaSprint1WhitelabelAPI/controller/ServiceFeedbackController.java
@@ -1,4 +1,71 @@
 package br.com.fiap.sprint1.JavaSprint1WhitelabelAPI.controller;
 
+import br.com.fiap.sprint1.JavaSprint1WhitelabelAPI.dto.serviceFeedback.CreateServiceFeedbackDTO;
+import br.com.fiap.sprint1.JavaSprint1WhitelabelAPI.dto.serviceFeedback.ServiceFeedbackDetailsDTO;
+import br.com.fiap.sprint1.JavaSprint1WhitelabelAPI.dto.serviceFeedback.UpdateServiceFeedbackDTO;
+import br.com.fiap.sprint1.JavaSprint1WhitelabelAPI.model.ServiceFeedBack;
+import br.com.fiap.sprint1.JavaSprint1WhitelabelAPI.service.ServiceFeedbackService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/servicefeedbacks")
 public class ServiceFeedbackController {
+
+    @Autowired
+    ServiceFeedbackService serviceFeedbackService;
+
+    @PostMapping("employee/{employee_id}")
+    public ResponseEntity<ServiceFeedbackDetailsDTO> create(
+            @PathVariable("employee_id") Long employeeId,
+            @RequestBody CreateServiceFeedbackDTO serviceFeedbackDTO,
+            UriComponentsBuilder uri
+    ){
+        ServiceFeedBack serviceFeedBack = serviceFeedbackService.create(employeeId, serviceFeedbackDTO);
+        var url = uri.path("employee/{employee_id}").buildAndExpand(serviceFeedBack.getId()).toUri();
+
+        return ResponseEntity.created(url).body(new ServiceFeedbackDetailsDTO(serviceFeedBack));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ServiceFeedbackDetailsDTO>> findAll(){
+        var serviceFeedbackList = serviceFeedbackService.getAll();
+
+        return ResponseEntity.ok(serviceFeedbackList);
+    }
+
+
+    @GetMapping("{service_feedback_id}")
+    public ResponseEntity<ServiceFeedbackDetailsDTO> findOne(
+            @PathVariable("service_feedback_id") Long serviceFeedbackId
+            ){
+        var serviceFeedback = serviceFeedbackService.getOne(serviceFeedbackId);
+
+        return ResponseEntity.ok(serviceFeedback);
+    }
+
+    @PostMapping("{service_feedback_id}/employee/{employee_id}")
+    public ResponseEntity<ServiceFeedbackDetailsDTO> update(
+            @PathVariable("service_feedback_id") Long serviceFeedbackId,
+            @PathVariable("employee_id") Long employeeId,
+            @RequestBody UpdateServiceFeedbackDTO serviceFeedbackDTO
+            ) {
+        var serviceFeedback = serviceFeedbackService.update(serviceFeedbackId, employeeId, serviceFeedbackDTO);
+
+        return ResponseEntity.ok(serviceFeedback);
+    }
+
+
+    @DeleteMapping("{service_feedback_id}")
+    public ResponseEntity<Void> delete(
+            @PathVariable("service_feedback_id") Long serviceFeedbackId
+    ){
+        serviceFeedbackService.delete(serviceFeedbackId);
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- src/main/java/br/com/fiap/sprint1/JavaSprint1WhitelabelAPI/service/ResponseService.java
+++ src/main/java/br/com/fiap/sprint1/JavaSprint1WhitelabelAPI/service/ResponseService.java
@@ -1,4 +1,41 @@
 package br.com.fiap.sprint1.JavaSprint1WhitelabelAPI.service;
 
+import br.com.fiap.sprint1.JavaSprint1WhitelabelAPI.dto.response.ResponseDetaisDTO;
+import br.com.fiap.sprint1.JavaSprint1WhitelabelAPI.model.Response;
+import br.com.fiap.sprint1.JavaSprint1WhitelabelAPI.repository.ResponseRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ResponseService {
+
+    @Autowired
+    ResponseRepository responseRepository;
+
+    @Transactional
+    public ResponseDetaisDTO create() {
+
+    }
+
+    public List<ResponseDetaisDTO> getAll() {
+        List<ResponseDetaisDTO> responseList = responseRepository.findAll()
+                .stream().map(ResponseDetaisDTO::new).toList();
+
+        return responseList;
+    }
+
+    public ResponseDetaisDTO getOne(Long responseId) {
+       Response response = responseRepository.getReferenceById(responseId);
+
+       return new ResponseDetaisDTO(response);
+    }
+
+    @Transactional
+    public void delete(Long responseId) {
+        responseRepository.deleteById(responseId);
+    }
+
 }
