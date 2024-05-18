76b16ab528f153e91775f0e59e2e48c650c23506
diogo_gallina <diogogallina20@gmail.com>
2024-05-17 22:53:41 -0300

    fix: resolve constrait erro in create reclamation route

--- src/main/java/br/com/fiap/sprint1/JavaSprint1WhitelabelAPI/dto/enterprise/UpdateEnterpriseDTO.java
+++ src/main/java/br/com/fiap/sprint1/JavaSprint1WhitelabelAPI/dto/enterprise/UpdateEnterpriseDTO.java
@@ -9,8 +9,6 @@ public record UpdateEnterpriseDTO(
         @Size(max = 50, message = "Nome pode ter no máximo 50 caracteres")
         String name,
 
-        @NotBlank(message = "Tipo de segmento não pode ser vazio")
-        @Size(max = 50, message = "Tipo de segmento pode ter no máximo 50 caracteres")
         SegmentType segmentType
 ){
 }
--- src/main/java/br/com/fiap/sprint1/JavaSprint1WhitelabelAPI/dto/reclamation/UpdateReclamationDTO.java
+++ src/main/java/br/com/fiap/sprint1/JavaSprint1WhitelabelAPI/dto/reclamation/UpdateReclamationDTO.java
@@ -3,6 +3,7 @@ package br.com.fiap.sprint1.JavaSprint1WhitelabelAPI.dto.reclamation;
 import br.com.fiap.sprint1.JavaSprint1WhitelabelAPI.model.enums.ReclamationState;
 import br.com.fiap.sprint1.JavaSprint1WhitelabelAPI.model.enums.ReclamationType;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.util.List;
@@ -16,15 +17,13 @@ public record UpdateReclamationDTO(
         @Size(max = 250, message = "Texto pode ter no máximo 250 caracteres")
         String text,
 
-        @NotBlank(message = "Tipo de Reclamação não pode ser vazio")
-        @Size(max = 70, message = "Tipo de Reclamação pode ter no máximo 70 caracteres")
+        @NotNull(message = "Tipo de Reclamação não pode ser vazio")
         ReclamationType type,
 
-        @NotBlank(message = "Estado da reclamação não pode ser vazio")
-        @Size(max = 30, message = "Estado da reclamação pode ter no máximo 30 caracteres")
+        @NotNull(message = "Estado da reclamação não pode ser vazio")
         ReclamationState state,
 
-        @NotBlank(message = "Lista de Ids de funcionários não pode ser vazia")
+        @NotNull(message = "Lista de Ids de funcionários não pode ser vazia")
         List<Long> employeeIds
 ) {
 }
